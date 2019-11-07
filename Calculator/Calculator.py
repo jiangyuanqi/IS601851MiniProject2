20772ce5190bc629e7818ab73ff97e5cfb2942ca
Yuanqi Jiang <yj99@njit.edu>
2019-11-07 08:05:42 -0500

    Feature: population_variance

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -76,39 +76,19 @@ def confidence_interval(a, b, c):
     a = int(a)
     b = int(b)
     c = int(c)
-    d = statistics.stdev([a, b, c])
-    e = ( a + b + c) / 3
-    f = ((a - e)/ d +(b - e)/ d +(c - e)/ d)/ 3
+    d = zscore(a, b, c)
+    e = stdev(a, b, c)
+    f = d * e / Decimal(math.sqrt(3)).quantize(Decimal('.001'))
     return Decimal(f).quantize(Decimal('.001'))
 
-'''
 
-
-def multiplication(a, b):
+def population_variance(a, b, c):
     a = int(a)
     b = int(b)
-    c = b * a
-    return c
-
-
-def division(a, b):
-    a = float(a)
-    b = float(b)
-    c = b / a
-    return Decimal(c).quantize(Decimal('.000000001'))
-
-
-def square(a):
-    a = int(a)
-    c = a * a
-    return c
-
-
-def square_root(a):
-    a = int(a)
-    c = math.sqrt(a)
-    return Decimal(c).quantize(Decimal('.000001'))
-'''
+    c = int(c)
+    d = stdev(a, b, c)
+    e = d * d
+    return Decimal(e).quantize(Decimal('.001'))
 
 
 class Calculator:
@@ -153,16 +133,7 @@ class Calculator:
         self.result = confidence_interval(a, b, c)
         return self.result
 
-    '''
-    def division(self, a, b):
-        self.result = division(a, b)
+    def population_variance(self, a, b, c):
+        self.result = confidence_interval(a, b, c)
         return self.result
 
-    def square(self, a):
-        self.result = square(a)
-        return self.result
-
-    def square_root(self, a):
-        self.result = square_root(a)
-        return self.result
-    '''
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -9,6 +9,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
+    '''
     def test_mean(self):
         test_data = CsvReader('/Tests/Data/unit_test_mean.csv').data
         for row in test_data:
@@ -21,19 +22,19 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             self.assertEqual(self.calculator.median(row['Value 1'],row['Value 2'],row['Value 3']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
+            
     def test_mode(self):
         test_data = CsvReader('/Tests/Data/unit_test_mode.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.mode(row['Value 1'],row['Value 2'],row['Value 3']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
+            
     def test_stdev(self):
         test_data = CsvReader('/Tests/Data/unit_test_stdev.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.stdev(row['Value 1'],row['Value 2'],row['Value 3']), Decimal(row['Result']).quantize(Decimal('.001')))
             self.assertEqual(self.calculator.result, Decimal(row['Result']).quantize(Decimal('.001')))
-
+            
     def test_variance(self):
         test_data = CsvReader('/Tests/Data/unit_test_variance.csv').data
         for row in test_data:
@@ -52,7 +53,7 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             self.assertEqual(self.calculator.standardized_score(row['Value 1'],row['Value 2'],row['Value 3']), Decimal(row['Result']).quantize(Decimal('.001')))
             self.assertEqual(self.calculator.result, Decimal(row['Result']).quantize(Decimal('.001')))
-
+ 
     def test_population_correlation_coefficient(self):
         test_data = CsvReader('/Tests/Data/unit_test_population_correlation_coefficient.csv').data
         for row in test_data:
@@ -60,18 +61,23 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, Decimal(row['Result']).quantize(Decimal('.001')))
 
 
-
- '''
-
-
-Confidence Interval
-Population Variance
-P Value
-Proportion
-Sample Mean
-Sample Standard Deviation
-Variance of sample proportion
+    def test_confidence_interval(self):
+        test_data = CsvReader('/Tests/Data/unit_test_confidence_interval.csv').data
+        for row in test_data:
+            self.assertEqual(
+                self.calculator.confidence_interval(row['Value 1'], row['Value 2'], row['Value 3']),
+                Decimal(row['Result']).quantize(Decimal('.001')))
+            self.assertEqual(self.calculator.result, Decimal(row['Result']).quantize(Decimal('.001')))
 '''
+
+    def test_population_variance(self):
+        test_data = CsvReader('/Tests/Data/unit_test_population_variance.csv').data
+        for row in test_data:
+            self.assertEqual(
+                self.calculator.population_variance(row['Value 1'], row['Value 2'], row['Value 3']),
+                Decimal(row['Result']).quantize(Decimal('.001')))
+            self.assertEqual(self.calculator.result, Decimal(row['Result']).quantize(Decimal('.001')))
+
 
 if __name__ == '__main__':
     unittest.main()
